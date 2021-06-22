adca21da8e8d26448d08691ab5c122115f800007
Charles Gallay <cg@visium.ch>
2021-06-22 09:31:39 +0200

    fix issues

--- images/mnist/mnist.py
+++ images/mnist/mnist.py
@@ -1,7 +1,11 @@
 """mnist dataset."""
 
 from dvc.api import get_url
+import dvc
 import tensorflow_datasets as tfds
+import subprocess
+import tensorflow as tf
+import numpy as np
 
 # TODO(mnist): Markdown description  that will appear on the catalog page.
 _DESCRIPTION = """
@@ -14,9 +18,11 @@ It should also contain any processing which has been applied (if any),
 # TODO(mnist): BibTeX citation
 _CITATION = """
 """
+_MNIST_IMAGE_SIZE = 28
+MNIST_IMAGE_SHAPE = (_MNIST_IMAGE_SIZE, _MNIST_IMAGE_SIZE, 1)
+MNIST_NUM_CLASSES = 10
 
-
-class Mnist(tfds.core.GeneratorBasedBuilder):
+class MyMnist(tfds.core.GeneratorBasedBuilder):
   """DatasetBuilder for mnist dataset."""
 
   VERSION = tfds.core.Version('1.0.0')
@@ -25,43 +31,79 @@ class Mnist(tfds.core.GeneratorBasedBuilder):
   }
 
   def _info(self) -> tfds.core.DatasetInfo:
-    """Returns the dataset metadata."""
-    # TODO(mnist): Specifies the tfds.core.DatasetInfo object
-    return tfds.core.DatasetInfo(
-        builder=self,
-        description=_DESCRIPTION,
-        features=tfds.features.FeaturesDict({
-            # These are the features of your dataset like images, labels ...
-            'image': tfds.features.Image(shape=(None, None, 3)),
-            'label': tfds.features.ClassLabel(names=['no', 'yes']),
-        }),
-        # If there's a common (input, target) tuple from the
-        # features, specify them here. They'll be used if
-        # `as_supervised=True` in `builder.as_dataset`.
-        supervised_keys=('image', 'label'),  # Set to `None` to disable
-        homepage='https://dataset-homepage/',
-        citation=_CITATION,
-    )
+      return tfds.core.DatasetInfo(
+          builder=self,
+          description=("The MNIST database of handwritten digits."),
+          features=tfds.features.FeaturesDict({
+              "image": tfds.features.Image(shape=MNIST_IMAGE_SHAPE),
+              "label": tfds.features.ClassLabel(num_classes=MNIST_NUM_CLASSES),
+          }),
+          supervised_keys=("image", "label"),
+          homepage="http://yann.lecun.com/exdb/mnist/",
+          citation="",
+      )
 
   def _split_generators(self, dl_manager: tfds.download.DownloadManager):
     """Returns SplitGenerators."""
     # TODO(mnist): Downloads the data and defines the splits
-    path = dl_manager.download_and_extract('gs://')
-
-    # TODO(mnist): Returns the Dict[split names, Iterator[Key, Example]]
-    return {
-        'train': self._generate_examples(path / 'train_imgs'),
-    }
-
-  def _generate_examples(self, path):
-    """Yields examples."""
-    # TODO(mnist): Yields (key, example) tuples from the dataset
-    for f in path.glob('*.jpeg'):
-      yield 'key', {
-          'image': f,
-          'label': 'yes',
-      }
+    # data_url = get_url("images/mnist/data", repo="https://github.com/VisiumCH/metrohm-dvc-demo")
 
 
+    # Make use of dvc to get the data instead of dl_manager
+    path = "data"
+    subprocess.call(["dvc", "pull", path])
 
-# get_url("")
+    return [
+        tfds.core.SplitGenerator(
+            name=tfds.Split.TRAIN,
+            gen_kwargs=dict(
+                num_examples=60_000,
+                data_path="data/train-images-idx3-ubyte",
+                label_path="data/train-labels.idx1-ubyte",
+            )),
+        tfds.core.SplitGenerator(
+            name=tfds.Split.TEST,
+            gen_kwargs=dict(
+                num_examples=10_000,
+                data_path="data/t10k-images.idx3-ubyte",
+                label_path="data/t10k-labels.idx1-ubyte",
+            )),
+    ]
+
+
+
+  def _generate_examples(self, num_examples, data_path, label_path):
+    """Generate MNIST examples as dicts.
+    Args:
+      num_examples (int): The number of example.
+      data_path (str): Path to the data files
+      label_path (str): Path to the labels
+    Yields:
+      Generator yielding the next examples
+    """
+    images = _extract_mnist_images(data_path, num_examples)
+    labels = _extract_mnist_labels(label_path, num_examples)
+    data = list(zip(images, labels))
+
+    # Using index as key since data is always loaded in same order.
+    for index, (image, label) in enumerate(data):
+      record = {"image": image, "label": label}
+      yield index, record
+
+
+def _extract_mnist_images(image_filepath, num_images):
+  with tf.io.gfile.GFile(image_filepath, "rb") as f:
+    f.read(16)  # header
+    buf = f.read(_MNIST_IMAGE_SIZE * _MNIST_IMAGE_SIZE * num_images)
+    data = np.frombuffer(
+        buf,
+        dtype=np.uint8,
+    ).reshape(num_images, _MNIST_IMAGE_SIZE, _MNIST_IMAGE_SIZE, 1)
+    return data
+
+def _extract_mnist_labels(labels_filepath, num_labels):
+  with tf.io.gfile.GFile(labels_filepath, "rb") as f:
+    f.read(8)  # header
+    buf = f.read(num_labels)
+    labels = np.frombuffer(buf, dtype=np.uint8).astype(np.int64)
+    return labels
